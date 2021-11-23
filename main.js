9ae4c335147124805c75ddd84b67b4ebd95e36f5
Ras Rossetti <rasrossetti@gmail.com>
2021-11-22 21:40:05 -0600

    Nav Work

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+//Menu 
+const hamburger = document.querySelector(".hamburger");
+const backdrop = document.querySelector(".backdrop2");
+const sidebar = document.querySelector(".sidebar2");
+
+hamburger.addEventListener("click", () => {
+  toggleSlidebar();
+});
+backdrop.addEventListener("click", () => {
+  toggleSlidebar();
+});
+
+const toggleSlidebar = () => {
+  backdrop.classList.toggle("backdrop2--active");
+  sidebar.classList.toggle("sidebar2--active");
+}
+
 //Sliderbase
 
 var slideNo = 1;
