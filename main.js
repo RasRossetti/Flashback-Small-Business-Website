362557c5196a939a58bbb77e47e204cb8d66e6bb
Ras Rossetti <rasrossetti@gmail.com>
2021-11-28 21:24:17 -0600

    CSS  and Function changes to Nav

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+//Nav Menu
+
+const hamburger = document.querySelector(".hamburger");
+const backdrop = document.querySelector(".backdrop2");
+const sidebar = document.querySelector(".sidebar2");
+const times = document.querySelector(".times");
+
+hamburger.addEventListener("click", () => {
+  toggleSlidebar();
+});
+backdrop.addEventListener("click", () => {
+  toggleSlidebar();
+});
+times.addEventListener("click", () => {
+  toggleSlidebar();
+});
+
+const toggleSlidebar = () => {
+  backdrop.classList.toggle("backdrop2--active");
+  sidebar.classList.toggle("sidebar2--active");
+}
+
 //Sliderbase
 
 var slideNo = 1;
