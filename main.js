9eda2e97e5cd4626470642dfadd9197c8083f548
Ras Rossetti <rasrossetti@gmail.com>
2022-03-02 16:35:02 -0600

    Review4

--- main.js
+++ main.js
@@ -60,39 +60,6 @@ function currentSlide(n) {
 }
 
 
-//slider1
-
-var slideNo1 = 1;
-Carousel1(slideNo1);
-
-function Carousel1(n) {
-  var slides1 = document.getElementsByClassName("image-container1");
-  var dots1 = document.getElementsByClassName("dot1");
-  if (n > slides1.length) {
-    slideNo1 = 1;
-  }
-  if (n < 1) {
-    slideNo1 = slides1.length;
-  }
-  for (var i = 0; i < slides1.length; i++) {
-    slides1[i].style.display = "none";
-  }
-  slides1[slideNo1 - 1].style.display = "block";
-  for (var i = 0; i < dots1.length; i++) {
-    dots1[i].className = dots1[i].className.replace(" active", "");
-  }
-  dots1[slideNo1 - 1].className += " active";
-
-}
-
-function newSlide1(n) {
-  Carousel1(slideNo1 += n);
-}
-
-function currentSlide1(n) {
-  Carousel1(slideNo1 = n);
-}
-
 //slider2 
 
 var slideNo2 = 1;
@@ -123,33 +90,6 @@ function newSlide2(n) {
 }
 
 
-//slider3 
-
-var slideNo3 = 1;
-Carousel3(slideNo3);
-
-function Carousel3(n) {
-  var slides3 = document.getElementsByClassName("image-container3");
-  var dots3 = document.getElementsByClassName("dot3");
-  if (n > slides3.length) {
-    slideNo3 = 1;
-  }
-  if (n < 1) {
-    slideNo3 = slides3.length;
-  }
-  for (var i = 0; i < slides3.length; i++) {
-    slides3[i].style.display = "none";
-  }
-  slides3[slideNo3 - 1].style.display = "block";
-  for (var i = 0; i < dots3.length; i++) {
-    dots3[i].className = dots3[i].className.replace(" active", "");
-  }
-  dots3[slideNo3 - 1].className += " active";
-}
-function newSlide3(n) {
-  Carousel3(slideNo3 += n);
-}
-
 //slider4 
 
 var slideNo4 = 1;
@@ -177,30 +117,3 @@ function newSlide4(n) {
   Carousel4(slideNo4 += n);
 }
 
-
-//slider5
-
-var slideNo5 = 1;
-Carousel5(slideNo5);
-
-function Carousel5(n) {
-  var slides5 = document.getElementsByClassName("image-container5");
-  var dots5 = document.getElementsByClassName("dot5");
-  if (n > slides5.length) {
-    slideNo5 = 1;
-  }
-  if (n < 1) {
-    slideNo5 = slides5.length;
-  }
-  for (var i = 0; i < slides5.length; i++) {
-    slides5[i].style.display = "none";
-  }
-  slides5[slideNo5 - 1].style.display = "block";
-  for (var i = 0; i < dots5.length; i++) {
-    dots5[i].className = dots5[i].className.replace(" active", "");
-  }
-  dots5[slideNo5 - 1].className += " active";
-}
-function newSlide5(n) {
-  Carousel5(slideNo5 += n);
-}
